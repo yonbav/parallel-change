1b9e569f94e17a877c90f8b04b13f1014223f3b3
Oleksii Fedorov <waterlink000@gmail.com>
2016-04-15 18:41:22 +0200

    Add Swift version

---
+++ Swift/ParallelChange/ParallelChangeTest/AuthenticationService.swift
@@ -0,0 +1,43 @@
+import XCTest
+
+class AuthenticationService {
+
+    //
+    // the goal is to replace the method above with this one:
+    // func isAuthenticated(role, userId)
+    //
+
+    func isAuthenticated(userId: Int) -> Bool {
+        return userId == 12345
+    }
+}
+
+class AuthenticationClient {
+    let authenticationService: AuthenticationService
+
+    init(authenticationService: AuthenticationService) {
+        self.authenticationService = authenticationService
+    }
+
+    func run() {
+        let authenticated = authenticationService.isAuthenticated(33)
+        print("authenticated: \(authenticated)")
+    }
+}
+
+class JetAnotherClient {
+    func run() {
+        AuthenticationService().isAuthenticated(100)
+    }
+}
+
+class AuthenticationServiceMain: XCTestCase {
+    func runExample() {
+        let client = AuthenticationClient(authenticationService: AuthenticationService())
+        client.run()
+    }
+
+    func testRunExample() {
+        runExample()
+    }
+}
